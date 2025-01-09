ac1ce3af0ffacea6a96bb20fb370f9c0decbf2b2
tagnam <ravi@tagnamcommunications.com>
2025-01-09 16:10:00 +0530

    connect all page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ServiceComponent } from './service/service.component';
-import path from 'path';
-import { Component } from '@angular/core';
+
 
 
 export const routes: Routes = [
