095ea974b7e1c31930d99735894c4d9d5e0a05ba
tagnam <ravi@tagnamcommunications.com>
2025-01-09 16:25:58 +0530

    correct nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,6 @@ export const routes: Routes = [
 
     {path:"", component:HomeComponent},
     {path:"about", component:AboutComponent},
-    {path:"contact", component:AboutComponent},
-    {path:"service", component:AboutComponent}
+    {path:"contact", component:ContactComponent},
+    {path:"service", component:ServiceComponent}
 ];
